5681c44db5c00653cc2b6363f675b27b176adaa1
DeveloperMatheus97 <matheuscaitanosouza18@outlook.com>
2022-05-10 11:55:14 -0300

    first commit

---
+++ Ex10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("==================== TABUADA ====================");
+            Console.WriteLine("DIGITE UM NUMERO, PARA SABER SUA TABUADA");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = i * n;
+                Console.WriteLine(n + "x" + i + " = " + resultado);
+            }
+        }
+    }
+}
